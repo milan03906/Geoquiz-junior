319ab2dbda6c152d5c61768485c327f1a414d2fa
milan03906 <ericmilan03@gmail.com>
2026-05-11 16:00:03 +0200

    Delete api directory

--- api/middleware/auditLog.js
+++
@@ -1,9 +0,0 @@
-import AuditLog from "../models/AuditLog.js";
-
-export async function logAction(userId, action, details = "", ip = "") {
-  try {
-    await AuditLog.create({ user: userId, action, details, ip });
-  } catch (err) {
-    console.error("Audit log greška:", err.message);
-  }
-}
